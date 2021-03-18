78c5242bb4451bb9928cf2e1657e8d615edbf66a
Kyle Lemire <hello@klemire.com>
2021-03-18 12:54:40 -0500

    did a bit of cleanup and added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Andoubot server
+
+A very basic server that listens to Twitch chat and handles certain chat commands through Socket.io
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const tmi = require("tmi.js");
 const express = require("express");
 const app = express();
-
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
   cors: {
